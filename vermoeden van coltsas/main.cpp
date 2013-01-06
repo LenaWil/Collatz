2f6cd1715b5ad15117915be659649b7ed17a802b
Tijmen Wildervanck <tijmenwildervanck@MacBook-van-Tijmen-Wildervanck.local>
2013-01-06 19:06:53 +0100

    Initial Commit

---
+++ vermoeden van coltsas/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  vermoeden van coltsas
+//
+//  Created by Tijmen Wildervanck on 06-01-13.
+//  Copyright (c) 2013 Het Veldhuis. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
