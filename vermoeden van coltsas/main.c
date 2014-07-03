6d74f3c8992901081b83f0bdfb8c1ffb8cf6e281
Tijmen Wildervanck <tijmen.wildervanck@gmail.com>
2014-07-03 21:15:23 +0200

    uitlijning code

--- vermoeden van coltsas/main.c
+++ vermoeden van coltsas/main.c
@@ -11,34 +11,33 @@
 
 int main ()
 {
-
-    unsigned int keren=0;
-    unsigned long long int getal=0;
-    //unsigned char oneven;
-    printf("\nVoer een getal in(kan maximaal %llu zijn (2^%lu-1), en gebruik geen '.' of '-'):", ULONG_LONG_MAX, (sizeof(unsigned long long int)<<3));
+	unsigned int keren=0;
+	unsigned long long int getal=0;
+	//unsigned char oneven;
+	printf("\nVoer een getal in(kan maximaal %llu zijn (2^%lu-1), en gebruik geen '.' of '-'):", ULONG_LONG_MAX, (sizeof(unsigned long long int)<<3));
 begin:
 	scanf("%llu", &getal);
-    //getal=27;
-    
-    if (getal==0) {
-        printf("geef geen 0 op:");
-        goto begin;
-    }
-    while (getal!= 1) {
-        printf("getal %d\t=%llu\n", keren++, getal);
-        //oneven= ;
-        if ((getal & 1)== 1) {
-            if (ULONG_LONG_MAX/3<=getal) {
-                printf("getal %d: ?(getal te hoog)\nHet Programma sluit zich nu af", keren++);
-                return -2;
-            }
-            getal=(getal*3+1);}
-        else {
-            getal>>=1;
-    }
-    }
-    printf("getal %d\t=%llu\nHet duurde %d keer\n", keren, getal, keren);
-    return keren;
+	//getal=27;
+	
+	if (getal==0) {
+		printf("geef geen 0 op:");
+		goto begin;
+	}
+	while (getal!= 1) {
+		printf("getal %d\t=%llu\n", keren++, getal);
+		//oneven= ;
+		if ((getal & 1)== 1) {
+			if (ULONG_LONG_MAX/3<=getal) {
+				printf("getal %d: ?(getal te hoog)\nHet Programma sluit zich nu af", keren++);
+				return -2;
+			}
+			getal=(getal*3+1);}
+		else {
+			getal>>=1;
+	}
+	}
+	printf("getal %d\t=%llu\nHet duurde %d keer\n", keren, getal, keren);
+	return keren;
 }
 
 
